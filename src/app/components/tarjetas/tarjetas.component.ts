fda692795ae5e7b5e1655740f22c08579c173f56
Isaac Yriarte <isaacyagi@gmail.com>
2020-05-26 18:49:09 -0500

    Agrega el envio de datos y navegacion a la pagina de artistas

--- src/app/components/tarjetas/tarjetas.component.ts
+++ src/app/components/tarjetas/tarjetas.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-tarjetas',
@@ -9,9 +11,22 @@ export class TarjetasComponent implements OnInit {
 
   @Input() items: any[] = [];
   
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  verArtista(item: any){
+    let artistaId;
+
+    if (item.type === 'artist')
+      artistaId = item.id;
+    if (item.type === 'album')
+      artistaId = item.artists[0].id;
+    
+    this.router.navigate(['/artist',artistaId]);
   }
 
 }
