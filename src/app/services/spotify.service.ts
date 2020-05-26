f2c3c8267649cb08381457d918197a8f1117d292
Isaac Yriarte <isaacyagi@gmail.com>
2020-05-26 17:25:04 -0500

    Busca artista por nombre

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -6,13 +6,22 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class SpotifyService {
 
+  token: string = 'TOKEN';
   constructor(private http: HttpClient) { }
 
   getNewReleases(){
     const headers = new HttpHeaders({
-      'Authorization': 'TOKEN'
+      'Authorization': `Bearer ${this.token}`
     });
 
     return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20',{headers: headers});
+  }
+
+  getArtista(termino: string){
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${this.token}`
+    });
+
+    return this.http.get(`https://api.spotify.com/v1/search?query=${termino}&type=artist&offset=0&limit=20`,{headers: headers});
   }
 }
