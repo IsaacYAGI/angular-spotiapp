77fa9b9357126069a51db060fda3178b465e6a8d
Isaac Yriarte <isaacyagi@gmail.com>
2020-05-26 21:16:22 -0500

    Agrega componente mensaje de error al artista

--- src/app/components/artista/artista.component.ts
+++ src/app/components/artista/artista.component.ts
@@ -12,32 +12,47 @@ export class ArtistaComponent implements OnInit {
   artista: any = {};
   loading:boolean;
   topTracks: any[] = [];
+  error: boolean;
+  mensajeError: string = "";
 
   constructor(
     private activeRoute: ActivatedRoute,
     private spotifyService: SpotifyService
   ) {
+    this.cargarVista();
+   }
+
+  ngOnInit(): void {
+  }
+
+  cargarVista(){
     this.loading = true;
+    this.error = false;
     this.activeRoute.params.subscribe( params => {
       console.log(params)
       this.getArtista(params['id']);
       this.getTopTracks(params['id']);
     })
-   }
-
-  ngOnInit(): void {
   }
 
   getArtista(id: string){
     this.spotifyService.getArtista(id).subscribe(data => {
       this.artista = data;
       this.loading = false;
+    },(err) => {
+      this.error = true;
+      this.mensajeError = err.error.error.message;
+      this.loading = false
     });
   }
 
   getTopTracks(id: string){
     this.spotifyService.getTopTracks(id).subscribe(data => {
       this.topTracks = data;
+    }, (err) => {
+      this.error = true;
+      this.mensajeError = err.error.error.message;
+      this.loading = false
     });
   }
 
