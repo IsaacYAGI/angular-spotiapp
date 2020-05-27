0287672e1fdf946a27e079d213e83a171da6d232
Isaac Yriarte <isaacyagi@gmail.com>
2020-05-26 19:09:16 -0500

    Mapea el artista enviado por parametros

--- src/app/components/artista/artista.component.ts
+++ src/app/components/artista/artista.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
   selector: 'app-artista',
@@ -8,15 +9,28 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ArtistaComponent implements OnInit {
 
+  artista: any = {};
+  loading:boolean;
+
   constructor(
-    private activeRoute: ActivatedRoute
+    private activeRoute: ActivatedRoute,
+    private spotifyService: SpotifyService
   ) {
+    this.loading = true;
     this.activeRoute.params.subscribe( params => {
       console.log(params)
+      this.getArtista(params['id']);
     })
    }
 
   ngOnInit(): void {
+  }
+
+  getArtista(id: string){
+    this.spotifyService.getArtista(id).subscribe(data => {
+      this.artista = data;
+      this.loading = false;
+    });
   }
 
 }
